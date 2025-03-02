f8b478ae46580946c1afd44ab8c68a0c2739c447
venkatagopikrishnaverriboina <vgopikrishna77@gmail.com>
2025-03-02 07:50:12 +0530

    Revert "test files"
    This reverts commit 2053607aba180ffed6b82d185cfaa51d749c4e9e.

--- CertificateServiceTest.java
+++
@@ -1,9 +0,0 @@
-package com.mphasis.elearning.servicetest;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-@ExtendWith(SpringExtension.class)
-public class CertificateServiceTest {
-@
-}
